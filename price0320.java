39ba57955ce2c1c2f89f41c99ea542529d047410
Rella-huang <yucih20061022@gmail.com>
2026-03-20 03:02:46 +0000

    update

---
+++ price0320.java
@@ -0,0 +1,19 @@
+package javaa;
+import java.util.Scanner;
+public class price0320{
+public static void main(String[] args){
+    Scanner scn= new Scanner(System.in);
+    int price;
+    int input;
+    int total;
+    system.out.println("請輸入價格,-1結束");
+    input=scn.nextInt();
+    while(input>=0){
+        price+=input;
+        input=scn.nextInt();
+
+    }
+    system.out.println(price);
+}
+
+}
